43cffe836e6d0fe55f2cf1bd743ca807876fbdb2
Pasindu Jayalal <PD294@canterbury.ac.uk>
2026-04-25 01:14:08 +0530

    chore: Changed  genimi model to flash

--- app/core/config.py
+++ app/core/config.py
@@ -14,7 +14,7 @@ TOP_K_RESULTS = 3
 UPLOAD_DIR = "app/data/uploads"
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY") or os.getenv("GOOGLE_API_KEY")
-LLM_MODEL = "gemini-3-flash-preview"
+LLM_MODEL = "gemini-2.5-flash"
 MAX_TOKENS = 500
 
 DEFAULT_FALLBACK_ANSWER = "I don't know"
